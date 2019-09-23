3201435c9ca3eedc6dc21404ff8150904a6a9fb3
Mads Bøgeskov <mbg@danskebank.dk>
2019-09-23 20:21:26 +0200

    Added more functionality

--- lib/fastlane/plugin/swiftlint_codequality/actions/swiftlint_codequality_action.rb
+++ lib/fastlane/plugin/swiftlint_codequality/actions/swiftlint_codequality_action.rb
@@ -13,21 +13,40 @@ module Fastlane
             .each
             .select { |l| l.include?("Warning Threshold Violation") == false }
             .map { |line|
-               filename, start, reason = line.match(/(.*\.swift):(\d+):\d+:\s*(.*)/).captures
+              filename, start, reason = line.match(/(.*\.swift):(\d+):\d+:\s*(.*)/).captures
   
+              # example: error: Type Name Violation: Type name should only contain alphanumeric characters: 'FILE' (type_name)
+
+              issue_type, failure_type, description, rule = reason.match(/(.*?):?\s(.*?):\s(.*)\((.*)\)/).captures
+
+              case issue_type
+              when 'error'
+                severity = 'critical'
+              when 'warning'
+                severity = 'minor'
+              else
+                severity = 'info'
+              end
+
                {
-                :description => reason, 
+                :type => "issue",
+                :check_name => failure_type.strip,
+                :description => description.strip, 
                 :fingerprint => Digest::MD5.hexdigest(line),
+                :severity => severity,
                 :location => {
-                  :path => "ios" + filename.sub(pwd, ''),
+                  :path => params[:prefix] + filename.sub(pwd, ''),
                   :lines => {
-                    :begin => start.to_i
+                    :begin => start.to_i,
+                    :end => start.to_i
                   }
                 }
               }
             }
             .to_a
             .to_json
+
+
 
         IO.write(params[:output], result)
 
@@ -61,6 +80,12 @@ module Fastlane
                                   env_name: "SWIFTLINT_CODEQUALITY_OUTPUT",
                                description: "The path to the generated output report",
                                   optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :prefix,
+                                  env_name: "SWIFTLINT_CODEQUALITY_PREFIX_PATH",
+                               description: "Used to prefix the path of a file. Usefull in e.g. React Native projects where the iOS project is in a subfolder",
+                                  optional: true,
+                             default_value: '',
                                       type: String)
         ]
       end
--- lib/fastlane/plugin/swiftlint_codequality/version.rb
+++ lib/fastlane/plugin/swiftlint_codequality/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module SwiftlintCodequality
-    VERSION = "1.0.0"
+    VERSION = "1.0.1"
   end
 end
